69e9231650efec1707dbc73ec75eef9792b9b7f5
Sebastian Staudt <koraktor@gmail.com>
2011-09-04 22:55:36 +0200

    Fixed current view for some cases

--- config/initializers/current_view.rb
+++ config/initializers/current_view.rb
@@ -4,6 +4,9 @@ module ActionController
 
     def render(*args)
       @current_view = args.first
+      if @current_view.is_a? Hash
+        @current_view = @current_view[:template] || action_name.to_sym
+      end
       super
     end
 
