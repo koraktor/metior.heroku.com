6e60fb7143f1ec603f0ac0eae34bac585a9eff1f
Sebastian Staudt <koraktor@gmail.com>
2011-10-18 16:22:27 +0200

    Fixed setting the current view name

--- config/initializers/current_view.rb
+++ config/initializers/current_view.rb
@@ -5,8 +5,10 @@ module ActionController
     def render(*args)
       @current_view = args.first
       if @current_view.is_a? Hash
-        @current_view = @current_view[:template] || action_name.to_sym
+        @current_view = @current_view[:template] || action_name
       end
+      @current_view = @current_view.to_sym
+
       super
     end
 
